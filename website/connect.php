2f5e5666f740bc0f992bac1a06b7432f5437e3a5
dapacheconunez <dapacheconunez@miners.utep.edu>
2017-11-22 20:40:32 -0700

    added connect.php

---
+++ website/connect.php
@@ -0,0 +1,26 @@
+<?php 
+//session_start();
+
+$errors = array();
+
+$host = 'localhost:3306';
+$user = 'root';
+$password = '';
+$database = 'mydb';
+
+//do not change 
+$table = "records";
+$salt = "123456789";//set 11/1/2017
+$hashtype = "md5";
+
+
+$db = mysqli_connect($host, $user, $password, $database);
+
+if (!$db) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo  mysqli_connect_errno() . PHP_EOL;
+    echo  mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+?>
