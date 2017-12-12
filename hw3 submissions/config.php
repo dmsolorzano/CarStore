7696bd6d6360d8a4bd9f32a84972c9ccf73df992
asantellanes <asantellanes@miners.utep.edu>
2017-12-12 12:58:46 -0700

    Delete config.php

--- hw3 submissions/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-   define('host', 'earth.cs.utep.edu');   		//database server host
-   define('username', 'asantellanes');   		//database username
-   define('password', 'cs5339!asantellanes');    //database password
-   define('database', 'asantellanes');   		//database name
-   $db = mysqli_connect(host,username,password,database); //connection.
-
-?>
